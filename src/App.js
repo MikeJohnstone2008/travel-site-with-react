80e1eeddedcbdb06cc600685ea236d055baf61f9
MikeJohnstone <michael.johnstone@libertymutual.com>
2017-09-27 20:50:00 -0700

    removed unneeded code

--- src/App.js
+++ src/App.js
@@ -56,13 +56,6 @@ class App extends Component {
         </div>
       </nav>
 
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
     </div>
     );
   }
